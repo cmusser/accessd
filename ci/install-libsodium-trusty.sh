05aeafeed9cdb620e294871f5e18cc5759bd1d9a
Chuck Musser <cmusser@sonic.net>
2018-08-30 16:24:45 -0700

    Add a script that installs the libsodium-dev package
    This is more involved than I'd thought. The Travis CI VMs are Ubuntu
    14.04, which does not have libsodium in the default repository.

---
+++ ci/install-libsodium-trusty.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:chris-lea/libsodium
+sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list
+sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list
+sudo apt-get update && sudo apt-get install libsodium-dev
+
