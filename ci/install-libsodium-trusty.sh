68340861a392c5f1a7e1842cbdc2a1526ce6a8bb
Chuck Musser <cmusser@sonic.net>
2018-08-30 17:00:09 -0700

    Remove Travis support -- not working currently

--- ci/install-libsodium-trusty.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-sudo add-apt-repository -y ppa:chris-lea/libsodium
-sudo echo "deb http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list
-sudo echo "deb-src http://ppa.launchpad.net/chris-lea/libsodium/ubuntu trusty main" >> /etc/apt/sources.list
-sudo apt-get update && sudo apt-get -y install libsodium-dev
-
